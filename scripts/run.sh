2f58ffc3541fb80cf88758827211dd7b366d1fdf
Mohamad Safadieh <me@msafadieh.com>
2020-06-29 16:19:38 -0400

    add absolute path slash

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,7 @@ export GIT_BRANCH="$3"
 export GITHUB_CONTEXT="$4"
 export GIT_TOKEN="$5"
 echo "Bumping version if necessary..."
-bash scripts/bump.sh
+bash /scripts/bump.sh
 export GIT_TOKEN="$6"
 echo "Packaging and pushing..."
-bash scripts/package.sh
+bash /scripts/package.sh
