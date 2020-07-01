b03582422c02a8de0ca6ac5a5a2f67f2b3870598
Mohamad Safadieh <me@msafadieh.com>
2020-07-01 10:03:31 -0400

    init helm and add git

--- setup/setup_helm.sh
+++ setup/setup_helm.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-apk add curl
+apk add curl git
 
 VERSION=2.16.9
 CHECKSUM=197b9338129000f5d085b37e93ae3bcdd785901356a426dfa1f948d6b7a5a744
@@ -20,6 +20,7 @@ mkdir helm
 mv "$FILENAME" helm/
 cd helm
 tar xvf "$FILENAME"
-cp "linux-$ARCH/helm" /usr/bin/
+cp "linux-$ARCH/{helm,tiller}" /usr/bin/
 cd ..
 rm -rf helm
+helm init --client-only
